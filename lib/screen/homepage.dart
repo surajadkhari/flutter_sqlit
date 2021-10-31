65e53ed0f60040f8e962c8b92a91a837206719c5
surajadkhari <surajadkhari@gmail.com>
2021-10-31 16:29:50 +0545

    add read,update functiom

---
+++ lib/screen/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/services/sql_service.dart
@@ -0,0 +1,62 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SqlService {
+//Open Database
+  Future<sql.Database> Db() async {
+    return sql.openDatabase('todo.db', version: 1,
+        //create database
+        onCreate: (sql.Database database, int verssion) async {
+      await createTables(database);
+
+      //Direct create table ko execution code use garna milcha but hamile different fuction bata call agreko
+      //   await database.execute("""CREATE TABLE Todo(
+      //   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      //   title TEXT,
+      //   description TEXT,
+      //   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      // )""");
+    });
+  }
+
+  //Create Table
+  Future createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE Todo(
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      title TEXT,
+      description TEXT,
+      createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )""");
+  }
+
+//Inset To Do
+  Future<int> insertTodo(String title, String description) async {
+    //Databse open garera table ma pass garne, Sqlservice() use garera Db lai call gareko
+    final _db = await SqlService().Db(); //_db =instace to open data
+    final todoData = {
+      'title': title,
+      'description': description,
+    };
+    final id = await _db.insert('Todo', todoData,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+//Read todo
+  Future<List<Map<String, dynamic>>> getTodo() async {
+    final _db = await SqlService().Db();
+    return _db.query('Todo', orderBy: 'id');
+  }
+
+//Update new Todo
+  Future<int> updateTodo(int id, String title, String description) async {
+    final _db = await SqlService().Db();
+    final newTodo = {
+      'title': title,
+      'description': description,
+      'createdAt': DateTime.now().toString()
+    };
+    final result =
+        await _db.update('Todo', newTodo, where: 'id =?', whereArgs: [id]);
+    return result;
+  }
+}
